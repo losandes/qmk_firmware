cec0647b44a6882bd153afbb4045a0e61843ea71
losandes <andes.collab@gmail.com>
2019-10-07 11:36:08 -0400

    Adds Alt Apps Support to open_app Function

--- keyboards/massdrop/alt/keymaps/mac_losandes/config.h
+++ keyboards/massdrop/alt/keymaps/mac_losandes/config.h
@@ -8,17 +8,19 @@
 // #include "timer.h"
 
 // Apps
-#define APP_IDE               "atom.app"
-#define APP_TRM               "Terminal.app"
-#define APP_BRS               "Firefox.app"
-#define APP_BRS_ALT           "Safari.app"
-#define APP_SLK               "Slack.app"
-#define APP_FND               "Finder.app"
-#define APP_MSG               "Messagges.app"
-#define APP_MAL               "Mail.app"
-#define APP_CAL               "Calendar.app"
-#define APP_ZOM               "zoom.us.app"
+#define APP_IDE_AT  "atom.app"
+#define APP_IDE_VS  "Visual Studio Code.app"
+#define APP_TRM     "Terminal.app"
+#define APP_BRS_FF  "Firefox.app"
+#define APP_BRS_SA  "Safari.app"
+#define APP_SLK     "Slack.app"
+#define APP_FND     "Finder.app"
+#define APP_MSG     "Messagges.app"
+#define APP_MAL     "Mail.app"
+#define APP_CAL     "Calendar.app"
+#define APP_ZOM     "zoom.us.app"
+#define NOALT__     "no-alt-app-defined"
 
 #define TAPPING_TOGGLE 2          //2 taps for TT (double-tap to toggle layer)
 
-bool open_app(keyrecord_t *record, char *appname);
+bool open_app(keyrecord_t *record, char *appname, char *altappname);
