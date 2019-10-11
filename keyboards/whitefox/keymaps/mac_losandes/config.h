e9a0973c596e12dfd49596246d7b621ebd4086c5
losandes <andes.collab@gmail.com>
2019-10-10 20:57:38 -0400

    Updates Whitefox App Hotkeys

--- keyboards/whitefox/keymaps/mac_losandes/config.h
+++ keyboards/whitefox/keymaps/mac_losandes/config.h
@@ -1,5 +1,6 @@
-#define TAPPING_TOGGLE 2   // 2 taps for TT (double-tap to toggle layer)
-#define LEADER_TIMEOUT 500 // The time to wait before processing a leader key combo
+#define BOOTKEY_HOLD_MS	2000 // The duration the boot key needs to be held for before it registers a reboot
+#define TAPPING_TOGGLE  2    // 2 taps for TT (double-tap to toggle layer)
+#define LEADER_TIMEOUT  500  // The time to wait before processing a leader key combo
 
 void open_alfred(void);
 void open_spotlight(void);
--- keyboards/whitefox/keymaps/mac_losandes/keymap.c
+++ keyboards/whitefox/keymaps/mac_losandes/keymap.c
@@ -50,7 +50,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,      KC_BSPC,     KC_DEL,  \
         CTL_CAP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,          KC_UP,   KC_PGDN, \
-        TT(1),   KC_LALT, KC_LGUI,                            KC_SPC,                    TT(3),   TT(2),                     KC_LEFT, KC_DOWN, KC_RGHT  \
+        TT(1),   KC_LALT, KC_LGUI,                            KC_SPC,                    KC_LEAD, TT(2),                     KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     /* Layer 1: Function Keys
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -190,7 +190,7 @@ void matrix_scan_user(void) {
         SEND_STRING("Firefox.app" SS_TAP(X_ENTER));
       }
 
-      SEQ_ONE_KEY(KC_BSLS) {
+      SEQ_ONE_KEY(KC_BSPC) {
         open_spotlight();
         SEND_STRING("Firefox.app" SS_TAP(X_ENTER));
       }
@@ -225,7 +225,7 @@ void matrix_scan_user(void) {
         SEND_STRING("Safari.app" SS_TAP(X_ENTER));
       }
 
-      SEQ_TWO_KEYS(KC_BSLS, KC_BSLS) {
+      SEQ_TWO_KEYS(KC_BSPC, KC_BSPC) {
         open_spotlight();
         SEND_STRING("Safari.app" SS_TAP(X_ENTER));
       }
